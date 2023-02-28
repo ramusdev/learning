9f71754ef5fa8138bcc0e49a041e181d6ad12764
Artuxa94 <arturdimitrov1994@gmail.com>
2023-02-28 13:58:25 +0100

    changeThirdStrings

--- lab1/lean1/src/Main.java
+++ lab1/lean1/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is Java project");
         System.out.println("Second string");
-        System.out.println("Third string is 3");
+        System.out.println("Third 1string");
     }
 }
