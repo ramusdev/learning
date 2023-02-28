0dbd4f8ddaf493cdcd3c34ec5180ca0852c37bef
ramusdev <ram219219@gmail.com>
2023-02-28 16:01:53 +0300

    Some change

--- lab1/lean1/src/Main.java
+++ lab1/lean1/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is Java project");
-        System.out.println("Second string");
-        System.out.println("Third 1string");
+        // System.out.println("This is Java project");
+        // System.out.println("Second string");
+
+        boolean a = true;
+
+        if (a) {
+            System.out.println("a is true");
+        } else {
+            System.out.println("a is false");
+        }
+
     }
 }
